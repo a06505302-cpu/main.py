b11c72a551e2a40ff71f43673497710df4e29467
a06505302-cpu <a06505302@gmail.com>
2026-03-24 16:00:01 +0200

    main.py

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ async def check_card_api(card_full):
     async with api_semaphore:
         try:
             async with httpx.AsyncClient(timeout=20) as client:
-                r = await client.get("https://mainpy-production-3201.up.railway.app/check", params=params)
+                r = await client.get("http://gatescheck.duckdns.org:7000/check", params=params)
                 result_raw = r.json().get('result','')
                 result = result_raw.lower()
                 if "charge" in result or "success" in result:
