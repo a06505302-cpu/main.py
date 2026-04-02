02b2795fd2658adb05f3377a9d32034788632f0c
a06505302-cpu <a06505302@gmail.com>
2026-04-02 20:44:37 +0200

    main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ ANTI_SPAM_SECONDS = 7
 
 GATES = [
     "https://rightchange.org/?give_forms=zakat",
-    "https://www.wfft.org/donations/general-donation/"
+    "https://www.wfft.org/donations/general-donation/",
     "https://raybensch.com/donations/support-ray/"
 ]
 gate_index = 0
